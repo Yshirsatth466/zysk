85b334b55a73179a1482ca0bcf98a504d6a0cbfd
Yshirsatth466 <108537307+Yshirsatth466@users.noreply.github.com>
2024-10-04 17:31:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="pl-80 pr-80 pt-20 pb-20">
+        <div className="">
+          <nav>
+            Image
+            <ul>
+              <li>Home</li>
+              <details>
+                <summary>
+
+                </summary>
+              </details>
+              <details>
+                <summary>
+
+                </summary>
+              </details>
+              <li>Pricing</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
